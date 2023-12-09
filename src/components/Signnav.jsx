e5844109c7a36469dbe12be8279e637ba9d05e6a
JBN303 <127194796+JBN303@users.noreply.github.com>
2023-12-09 14:24:17 +0530

    Add files via upload

---
+++ src/components/Signnav.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Signnav = () => {
+  return (
+    <div>
+        <table border="0" >
+          <tbody>
+                  <tr>
+              <td>
+                <input type="reset" value="Reset" className="login-btn btn-primary-soft btn" />
+              </td>
+              <td>
+                <input type="submit" value="" className="login-btn btn-primary btn" />
+              </td>
+            </tr>
+            </tbody>
+            </table>
+    </div>
+  )
+}
+
+export default Signnav
